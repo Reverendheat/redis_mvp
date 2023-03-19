42bf0f3196b5c3d1515de52bdcac85125cad257a
Reverendheat <sharp.brandon@gmail.com>
2023-03-19 14:03:21 -0400

    Add: docstring

--- redis_mvp/api/main.py
+++ redis_mvp/api/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 
-from redis_mvp.redis_worker.worker import start
-from redis_mvp.redis_worker.updater import Comment
+from redis_mvp.queue.worker import start
+from redis_mvp.queue.updater import Comment
 
 app = FastAPI()
 
 
 @app.post("/start")
 async def start_job():
+    """Kick off Redis Worker to retrieve and store data from API."""
     start()
     return {"status": "job started"}
 
 
 @app.get("/comments")
 async def get_comments():
+    """Retrieve all comments from Redis DB."""
     return {"comments": Comment.find().all()}
